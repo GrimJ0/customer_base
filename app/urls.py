42cad7da0f6b987871e5cb9d125fa3ef87a713c4
Vardan <kinlord@mail.ru>
2022-01-21 18:12:49 +0300

    feat: Добавление возможности редактировать клиентов

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomeList, NewClientView, RemoveClientView
+from .views import HomeList, NewClientView, EditRecipeView, RemoveClientView
 
 urlpatterns = [
     path("", HomeList.as_view(), name="home"),
     path("new/", NewClientView.as_view(), name="new"),
-    path("remove/<int:id>/", RemoveClientView.as_view(), name="remove_client")
+    path("edit/<int:id>/", EditRecipeView.as_view(), name="edit_client"),
+    path("remove/<int:id>/", RemoveClientView.as_view(), name="remove_client"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DeleteView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 
 from .servises import FormList
 from .forms import ClientForm
@@ -15,10 +15,20 @@ class HomeList(ListView):
     def get_queryset(self):
         return Client.objects.all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['home'] = True
+        return context
+
 
 class NewClientView(FormList, CreateView):
     form_class = ClientForm
     template_name = 'new_client.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['new_client'] = True
+        return context
 
     def form_valid(self, form):
         self.user_form_valid(self.request)
@@ -37,15 +47,24 @@ class NewClientView(FormList, CreateView):
         return super().form_valid(form)
 
 
+class EditRecipeView(UpdateView):
+    """Класс для редактирования рецепта"""
+    model = Client
+    form_class = ClientForm
+    template_name = 'new_client.html'
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['edit'] = True
+        return context
+
+    # def form_valid(self, form):
+    #     return super().user_form_valid(self.request, self.get_context_data, form)
+
+
 class RemoveClientView(DeleteView):
     model = Client
     template_name = 'remove_client.html'
     success_url = reverse_lazy('home')
     pk_url_kwarg = 'id'
-
-    def get(self, request, *args, **kwargs):
-        # client = Client.objects.get(id=self.get_object().id)
-        # client.delete()
-        print(1)
-        return super().get(request, *args, **kwargs)
-
